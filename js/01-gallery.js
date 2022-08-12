07e617563aaefddaf149778b783215854f33f3c7
Dmytro Voronievskyi <vdrabota@icloud.com>
2022-08-12 17:30:19 +0200

    Task1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -29,10 +29,29 @@ function onGalleryContainerClick(evt) {
   if (!evt.target.classList.contains("gallery__image")) {
     return;
   }
+  const modalImg = evt.target.dataset.source;
 
   const createModalItemGallery = basicLightbox.create(
-    `<img src="${evt.target.dataset.source}" width="800" height="600">`
+    `<img src="${modalImg}" width="800" height="600">`,
+    {
+      onShow: () => {
+        console.log("onShow", createModalItemGallery);
+        window.addEventListener("keydown", onEscPressKey);
+      },
+
+      onClose: () => {
+        console.log("onClose", createModalItemGallery);
+        window.removeEventListener("keydown", onEscPressKey);
+      },
+    }
   );
 
   createModalItemGallery.show();
+}
+
+function onEscPressKey(evt) {
+  if (evt.code === "Escape") {
+    console.log("hi");
+    createModalItemGallery.close();
+  }
 }
